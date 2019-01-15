696d0feab51db70c107a146baac7d8c5317c9ca8
Ryan Jenks <ryan.w.jenks@gmail.com>
2019-01-14 20:04:21 -0700

    Working webpage

---
+++ Music/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, url_for, jsonify, send_from_directory
+import os
+import requests
+import json
+
+app = Flask(__name__, template_folder='.')
+
+@app.route('/')
+def homepage():
+  return render_template('index.html')
+
+
+@app.route('/bar-hierarchy')
+def graph():
+  return render_template('bar-hierarchy.html')
+
+@app.route('/test.json')
+def get_file_content():
+  return send_from_directory('js', 'test.json')
+
+
+@app.route('/Data.html')
+def table():
+  return render_template('Data.html')
+# @app.route('/test.json')
+# def get_file_content():
+#   return send_from_directory('js', 'test.json')
+  
+
+
+if __name__ == '__main__':
+  app.run(host='127.0.0.1', debug=True)
