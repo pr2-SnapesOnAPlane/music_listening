a740dc7131daa58ba30cf9116ba11c5bd86b9d9b
tc-sabr <42155277+tc-sabr@users.noreply.github.com>
2019-01-16 13:16:56 -0700

    chartjs for sum of certified units

---
+++ cu_chart.js
@@ -0,0 +1,33 @@
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Album", "Single", "Shortform Album", "Video Longform"],
+        datasets: [{
+            label: 'Sum of Certified Units',
+            data: [6447, 3173, 2, 9.9],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
