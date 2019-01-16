4189d457208e5cc950ae488004c5ad0c83aa9f71
tc-sabr <42155277+tc-sabr@users.noreply.github.com>
2019-01-16 13:15:55 -0700

    chartjs for count of format type

---
+++ album_chart.js
@@ -0,0 +1,33 @@
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Album", "Single", "Shortform Album", "Video Longform"],
+        datasets: [{
+            label: 'Count of Format Type',
+            data: [1483, 849, 4, 1],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
