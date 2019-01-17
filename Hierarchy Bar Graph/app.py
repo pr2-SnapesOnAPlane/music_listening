67672f5b4ff3c70480a94d15e225a5df61c47c5e
Mollie <mollie.hannahw@gmail.com>
2019-01-16 18:36:25 -0700

    completed data included

--- Hierarchy Bar Graph/app.py
+++ Hierarchy Bar Graph/app.py
@@ -13,8 +13,8 @@ def homepage():
 @app.route('/test.json')
 def get_file_content():
   return send_from_directory('js', 'test.json')
+
+
   
-
-
 if __name__ == '__main__':
   app.run(host='127.0.0.1', debug=True)
